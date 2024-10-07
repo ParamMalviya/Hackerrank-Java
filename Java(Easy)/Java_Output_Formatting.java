afcc638be13fd53400df445a71369f32d17c6ccb
Param Malviya <150829778+ParamMalviya@users.noreply.github.com>
2024-10-07 16:20:13 +0530

    Update Java_Output_Formatting.java

--- Java(Easy)/Java_Output_Formatting.java
+++ Java(Easy)/Java_Output_Formatting.java
@@ -11,7 +11,7 @@ Each String will have a maximum of  alphabetic characters, and each integer will
 Output Format
 
 In each line of output there should be two columns:
-The first column contains the String and is left justified using exactly  characters.
+The first column contains the String and is left justified using exactly 15 characters.
 The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
 
 Sample Input
