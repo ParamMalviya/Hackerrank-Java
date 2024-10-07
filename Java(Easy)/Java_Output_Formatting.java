851a73ed395cb3db8447d799ec7926f6356c01d8
Param Malviya <150829778+ParamMalviya@users.noreply.github.com>
2024-10-07 16:20:41 +0530

    Update Java_Output_Formatting.java

--- Java(Easy)/Java_Output_Formatting.java
+++ Java(Easy)/Java_Output_Formatting.java
@@ -12,7 +12,7 @@ Output Format
 
 In each line of output there should be two columns:
 The first column contains the String and is left justified using exactly 15 characters.
-The second column contains the integer, expressed in exactly  digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+The second column contains the integer, expressed in exactly 3 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
 
 Sample Input
 
